f440ec4e03feb95f089065cb87083e6d7d12e9ca
sh-osama-sami <sherysamy169@gmail.com>
2024-03-04 19:14:23 +0200

    User read

--- bin/dbms.sh
+++ bin/dbms.sh
@@ -5,3 +5,14 @@ source ../lib/menu.sh
 #display the main menu
 display_main_menu
 
+read -p "Enter a number: " choice
+
+case $choice in 
+    1) echo "Create a new database";;
+    2) echo "List existing databases";;
+    3) echo "Drop a database";;
+    4) echo "Connect to a database";;
+    5) echo "Exit";;
+    *) echo "Invalid choice";;
+esac
+
